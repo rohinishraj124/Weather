5d0f76d378fd01f9205888535e5708a221156ac0
rohinishraj124 <rohinishraj124@gmail.com>
2024-07-07 14:33:24 +0530

    changed

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -13,21 +13,25 @@ function CurrentWeather({ data }) {
             </div>
             <p style={{ fontSize: "1.5em", fontWeight: "600" }}>Details : </p>
             <div className="detail">
-                <div className="detail-box">
-                    <div className="detail-content-value">{data.main.humidity}%</div>
-                    <div className="detail-content">Humidity</div>
+                <div className="detail-box-group">
+                    <div className="detail-box">
+                        <div className="detail-content-value">{data.main.humidity}%</div>
+                        <div className="detail-content">Humidity</div>
+                    </div>
+                    <div className="detail-box">
+                        <div className="detail-content-value">{data.main.pressure} mb</div>
+                        <div className="detail-content">Pressure</div>
+                    </div>
                 </div>
-                <div className="detail-box">
-                    <div className="detail-content-value">{data.main.pressure} mb</div>
-                    <div className="detail-content">Pressure</div>
-                </div>
-                <div className="detail-box">
-                    <div className="detail-content-value">{data.wind.speed} km/hr</div>
-                    <div className="detail-content">Wind</div>
-                </div>
-                <div className="detail-box">
-                    <div className="detail-content-value">{data.clouds.all} %</div>
-                    <div className="detail-content">Clouds</div>
+                <div className="detail-box-group">
+                    <div className="detail-box">
+                        <div className="detail-content-value">{data.wind.speed} km/hr</div>
+                        <div className="detail-content">Wind</div>
+                    </div>
+                    <div className="detail-box">
+                        <div className="detail-content-value">{data.clouds.all} %</div>
+                        <div className="detail-content">Clouds</div>
+                    </div>
                 </div>
             </div>
         </div>
