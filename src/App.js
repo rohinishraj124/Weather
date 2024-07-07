6c037f99de8c27cd9b710aa2aeadf133f0a12dfb
rohinishraj124 <146836591+rohinishraj124@users.noreply.github.com>
2024-07-07 11:09:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { useState } from 'react';
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
-  const [backgroundImage, setBackgroundImage] = useState(null);
+  const [backgroundImage, setBackgroundImage] = useState('https://media.istockphoto.com/id/1477837346/photo/blue-sky-background-with-white-clouds.webp?b=1&s=170667a&w=0&k=20&c=mrQSUQ9x8giDk_nqOTV61njngf1BXLCG7YClgSMR7xU=');
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
@@ -42,13 +42,13 @@ function App() {
       case '03d':
         return 'https://png.pngtree.com/background/20230530/original/pngtree-clouds-covered-the-sky-in-the-morning-scattered-clouds-covered-the-picture-image_2818771.jpg'
       case '04d':
-        return 'https://img.freepik.com/premium-photo/low-angle-view-sky_1048944-437358.jpg?size=626&ext=jpg&ga=GA1.1.2082370165.1716508800&semt=ais_user';
+        return 'https://media.istockphoto.com/id/522123182/photo/sunset-crepuscular-rays-pouring-though-scattered-clouds.webp?b=1&s=170667a&w=0&k=20&c=U-fSqc_3YKHvC_Qfx8E-q-4dIMazyZbHZ5zUileZya0=';
       case '09d':
         return 'https://c1.wallpaperflare.com/preview/963/100/993/rain-background-drop-weather.jpg'
       case '10d':
         return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDR-Bg6cafZlmQcyyQieMO7qKP20ZKiC4t2Q&s';
       case '11d':
-        return 'https://media.istockphoto.com/id/469850273/photo/lightning-with-dramatic-cloudscape.jpg?s=612x612&w=0&k=20&c=AwlD6sOgSOaX6MojEFnIiwzLMmX0WZ_bT-dTMgv-hXk=';
+        return 'https://images.ctfassets.net/4ivszygz9914/2d13fe1a-7836-4e25-856f-d598b18ef620/30b6625bad94463569be8c0a5c9e19be/e39b7004-1f41-4b62-bd07-1d142da23c5c.jpeg?fit=fill&w=1200';
       case '13d':
         return 'https://cdn.pixabay.com/photo/2016/11/26/21/48/trees-1861704_640.jpg';
       case '50d':
@@ -62,32 +62,33 @@ function App() {
       case '03n':
         return 'https://png.pngtree.com/background/20230530/original/pngtree-clouds-covered-the-sky-in-the-morning-scattered-clouds-covered-the-picture-image_2818771.jpg'
       case '04n':
-        return 'https://img.freepik.com/premium-photo/low-angle-view-sky_1048944-437358.jpg?size=626&ext=jpg&ga=GA1.1.2082370165.1716508800&semt=ais_user';
+        return 'https://media.istockphoto.com/id/522123182/photo/sunset-crepuscular-rays-pouring-though-scattered-clouds.webp?b=1&s=170667a&w=0&k=20&c=U-fSqc_3YKHvC_Qfx8E-q-4dIMazyZbHZ5zUileZya0=';
       case '09n':
         return 'https://c1.wallpaperflare.com/preview/963/100/993/rain-background-drop-weather.jpg'
       case '10n':
         return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDR-Bg6cafZlmQcyyQieMO7qKP20ZKiC4t2Q&s';
       case '11n':
-        return 'https://media.istockphoto.com/id/469850273/photo/lightning-with-dramatic-cloudscape.jpg?s=612x612&w=0&k=20&c=AwlD6sOgSOaX6MojEFnIiwzLMmX0WZ_bT-dTMgv-hXk=';
+        return 'https://images.ctfassets.net/4ivszygz9914/2d13fe1a-7836-4e25-856f-d598b18ef620/30b6625bad94463569be8c0a5c9e19be/e39b7004-1f41-4b62-bd07-1d142da23c5c.jpeg?fit=fill&w=1200';
       case '13n':
         return 'https://cdn.pixabay.com/photo/2016/11/26/21/48/trees-1861704_640.jpg';
       case '50n':
         return 'https://img.freepik.com/free-photo/vertical-shot-beautiful-green-trees-forest-foggy-table_181624-34182.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1719964800&semt=ais_user';
       default:
-        return null; // Default background if no match found
+        return 'https://img.freepik.com/free-photo/sky-dawn_1398-4604.jpg'; // Default background if no match found
     }
   };
 
   return (
+
     <div className='container_app' style={{
       backgroundImage: `url(${backgroundImage})`,
       backgroundSize: 'cover',
       backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center', // Center the background image
       minHeight: '100vh', // Ensure background covers entire viewport
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
-      backgroundColor: "skyblue"
     }}>
       <SearchBar onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
